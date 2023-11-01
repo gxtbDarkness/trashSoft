ec37ee141156036a6ee671602dd2897e36d1c614
gxtbDarkness <145451883+gxtbDarkness@users.noreply.github.com>
2023-11-01 11:05:12 +0800

    Delete hog_.py

--- hog_.py
+++
@@ -1,8 +0,0 @@
-import cv2
-from skimage.feature import hog
-
-
-def get_feature(image):
-    gray = cv2.resize(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), (64, 128))
-    hist = hog(gray, cells_per_block=(2, 2), block_norm='L2-Hys', transform_sqrt=True)
-    return hist
